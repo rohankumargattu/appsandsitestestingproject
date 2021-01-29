d7c966e4be4b18a5773f60912bf5efc11c5df110
gattu <gattu@DESKTOP-D8HNM4S>
2021-01-30 04:02:59 +0530

    ...

--- src/test/java/locators/MSMobileBy5AndroidViewTagAndroidAppEspresso.java
+++ src/test/java/locators/MSMobileBy5AndroidViewTagAndroidAppEspresso.java
@@ -1,9 +1,19 @@
 package locators;
 
+import io.appium.java_client.MobileBy;
+import io.appium.java_client.MobileSelector;
+
 public class MSMobileBy5AndroidViewTagAndroidAppEspresso
 {
 	public static void main(String[] args) throws Exception
 	{
-		//Refer notebook
+		/*
+		driver.findElement(MobileSelector.ANDROID_VIEWTAG.toString(),"view-tag attribute value").click();
+		driver.findElement(MobileBy.AndroidViewTag("view-tag attribute value")).click();
+		driver.findElementByAndroidViewTag("view-tag attribute value").click();
+		*/
+		
+		//view-tag attribute value can be given by Espresso driver only via getPageSoruce()
+		//Cannot be provided by Appium desktop server inspector/UiAutomatorViewer
 	}
 }
--- src/test/java/locators/MSMobileBy6AndroidDataMatcherAndroidAppEspresso.java
+++ src/test/java/locators/MSMobileBy6AndroidDataMatcherAndroidAppEspresso.java
@@ -1,9 +1,27 @@
 package locators;
 
+import io.appium.java_client.MobileBy;
+import io.appium.java_client.MobileSelector;
+
 public class MSMobileBy6AndroidDataMatcherAndroidAppEspresso
 {
 	public static void main(String[] args) throws Exception
 	{
-		//Refer Notebook
+		/*
+		driver.findElement(MobileSelector.ANDROID_DATA_MATCHER.toString(),"hamcrest matcher code").click();
+		driver.findElement(MobileBy.androidDataMatcher("hamcrest matcher code")).click();
+		driver.findElementByAndroidDataMatcher("hamcrest matcher code").click();
+		*/
+		
+		//Hancrest(JSON based JAVA) code
+		/*
+		{
+			"name":"hasEntry",
+			"args":["content-desc","End Call"]
+		}
+		*/
+		
+		//driver.findElementByAndroidDataMatcher("{\"name\":\"hasEntry\",\"args\":[\"content-desc\",\"End Call\"]}").click();
+		
 	}
 }
